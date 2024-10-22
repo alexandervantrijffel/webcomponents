f31b8ef680de1049d8bbd12b255f79180bd1408a
Alexander van Trijffel <alexander@structura.ws>
2024-10-22 11:46:30 +0200

    feat: calendar component skeleton with routing

--- crates/bin/serve/src/main.rs
+++ crates/bin/serve/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use askama::Template;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+use web_runner::axum_app_error::*;
+use web_runner::axum_server::add_middleware_layers;
+use web_runner::start_listenfd;
+use webcomponents::CalendarTemplate;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app_state = AppState::new().await?;
+
+    let router = add_middleware_layers(
+        Router::new()
+            .route("/", get(view_calendar))
+            // .route("/calendar", get(view_calendar::view_calendar))
+            .with_state(app_state),
+    );
+
+    axum::serve(start_listenfd("0.0.0.0:8181").await?, router)
+        .await
+        .context("Failed to run axum server")
+}
+
+#[derive(Clone)]
+pub struct AppState {}
+
+impl AppState {
+    async fn new() -> Result<Self> {
+        Ok(Self {})
+    }
+}
+
+pub async fn view_calendar() -> Result<impl IntoResponse, AppError> {
+    Ok(CalendarTemplate {}.render()?)
 }
--- crates/lib/web_runner/src/lib.rs
+++ crates/lib/web_runner/src/lib.rs
@@ -1,1 +1,16 @@
+pub mod axum_app_error;
 pub mod axum_server;
+
+pub use axum_app_error::AppError;
+use listenfd::ListenFd;
+use tokio::net::TcpListener;
+
+pub async fn start_listenfd(addr: &str) -> anyhow::Result<TcpListener> {
+    let mut listenfd = ListenFd::from_env();
+    if let Some(listener) = listenfd.take_tcp_listener(0)? {
+        let _ = listener.set_nonblocking(true);
+        Ok(TcpListener::from_std(listener)?)
+    } else {
+        Ok(TcpListener::bind(addr).await?)
+    }
+}
